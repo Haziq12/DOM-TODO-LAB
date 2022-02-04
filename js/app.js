8f5a71db3c600d80da74330c3c2bcfbaefe47310
Haziq Naeem <haziqnaeem36@gmail.com>
2022-02-03 19:06:26 -0500

    finish

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ const button = document.querySelector('#submit-button')
 const ul = document.querySelector('#todo-list')
 
 button.addEventListener('click', function(eval){
-  console.log(eval.target)
   let newLI = document.createElement('li')
   newLI.textContent = input.value 
   input.value = ''
